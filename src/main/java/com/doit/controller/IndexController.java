0657af8b407d4512471014d34ab27e99c46ba1c9
shuai <liershuai.cn@gmail.com>
2021-07-27 22:34:13 +0800

    首页前端
    Signed-off-by: shuai <liershuai.cn@gmail.com>

--- src/main/java/com/doit/controller/IndexController.java
+++ src/main/java/com/doit/controller/IndexController.java
@@ -1,13 +1,13 @@
-package com.doit;
+package com.doit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * <p>创建时间: @date 2021/07/24 下午 06:42 </p>
+ *
  * @author 李二帅
- * @version 1.0
- * @date 2021/07/24 下午 06:42
- * @since 8.0
+ * @version v1.0
  */
 @Controller
 @RequestMapping
