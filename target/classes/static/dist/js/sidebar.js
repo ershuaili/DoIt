a7cb847bdd09ee56023635ca5a73497b5bded03d
shuai <ershuaili.cn@gmail.com>
2021-10-07 17:25:01 +0800

    git

---
+++ target/classes/static/dist/js/sidebar.js
@@ -0,0 +1,58 @@
+$(function () {
+    // 侧边栏弹出
+    $(document).mouseup(function (e) {
+        let wrapper = $('#wrapper');
+        // 排除区域
+        let _con = $(".task,.taskInput,.layui-layer-shade,.layui-layer-page,.layui-layer-move,#calendar,.content,.bi");
+        if (!_con.is(e.target) && _con.has(e.target).length === 0) { // Mark 1
+            let left = $(wrapper)[0].offsetLeft;
+            if (left === 0) {
+                $(wrapper).offset({
+                    'left': 220
+                });
+                $('.headSculpture img').addClass('img');
+                $('.headSculpture p').addClass('opacity');
+                setTimeout(function () {
+                    $('.option ul>li').addClass('li');
+                }, 500)
+            } else {
+                $(wrapper).offset({
+                    'left': 0
+                });
+                setTimeout(function () {
+                    $('.headSculpture img').removeClass('img');
+                    $('.headSculpture p').removeClass('opacity');
+                    $('.option ul>li').removeClass('li');
+                }, 300)
+            }
+        }
+    })
+
+// 页面转跳
+    $('.option ul>li').click(function () {
+        let information = $(this).children().next().text();
+        switch (information) {
+            case "今天":
+                window.location.href = "/";
+                break;
+            case "明天":
+                window.location.href = "tom";
+                break;
+            case "这周":
+                window.location.href = "wee";
+                break;
+            case "日历":
+                window.location.href = "cal";
+                break;
+            case "任务":
+                window.location.href = "tas";
+                break;
+            case "清单":
+                window.location.href = "che";
+                break;
+            case "设置":
+                window.location.href = "set";
+                break;
+        }
+    });
+});
